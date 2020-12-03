4773088fd4ef732238f38eaf094ddec00c78062d
Simon Sandell <simonsandell94@gmail.com>
2020-12-03 20:47:32 +0100

    day 3 done

---
+++ 2020/03/test_one.py
@@ -0,0 +1,5 @@
+import one
+
+def test_check_tree():
+    assert one.check_if_pos_tree(3,0)
+    assert not one.check_if_pos_tree(0,0)
---
+++ 2020/03/test_two.py
@@ -0,0 +1,4 @@
+import two
+
+def test_treecount():
+    assert two.get_treecount(3,1) == 230
