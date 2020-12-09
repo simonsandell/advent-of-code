4261b91b0872987805846499a12f3ce42dc5dae8
Simon Sandell <simonsandell94@gmail.com>
2020-12-09 17:37:38 +0100

    fix day number

--- 2020/get_input.py
+++ 2020/get_input.py
@@ -5,7 +5,7 @@ with open('.secret', 'r') as f:
     secret = f.read().strip()
 headers = {'Cookie': secret}
 input = requests.get(
-        f'https://adventofcode.com/2020/day/{sys.argv[1]}/input',
+        f'https://adventofcode.com/2020/day/{int(sys.argv[1])}/input',
         headers=headers
         ).text
 with open(f'./{sys.argv[1]}/input', 'w') as f:
