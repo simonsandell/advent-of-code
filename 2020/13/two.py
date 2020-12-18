f0a0f4f5930fe292ed2db0c217ff265457b14d49
Simon Sandell <simonsandell94@gmail.com>
2020-12-18 22:38:58 +0100

    part 2 day 13

--- 2020/13/two.py
+++ 2020/13/two.py
@@ -1,6 +1,6 @@
 import math
 from functools import reduce
-with open('input') as f:
+with open('tinput') as f:
     input = f.read()
 
 def parse_input(input):
@@ -10,35 +10,50 @@ def parse_input(input):
             deps.append((int(x),i))
     return deps
 
-"""
-n_0*d_0 = x
-n_1*d_1 - 1 = x
-n_2*d_2 - 2 = x
-...
-
-n_1*d_1 = n_0*d_0 + 1
-"""
-
-
 deps = parse_input(input)
-deps.sort(key=0)
-print(deps)
-print(reduce(lambda x,y: x*y, map(lambda x: x[0],deps)))
-smallest = 100000000000000
+deps.sort(key=lambda x:x[0], reverse=True)
 
-def check_condition(x, dep, diff):
-    # dep*n = x + diff
-    # n = (x + diff)/dep
-    nl = math.floor((x+diff)/dep)
-    nu = math.ceil((x+diff)/dep)
-    if nl*dep -diff == x:
-        return True
-    if nu*dep -diff == x:
+
+def check_condition(t, dep, diff):
+    if (t + diff) % dep == 0:
         return True
     return False
 
-def get_x(n, dep, diff):
+def get_t(n, dep, diff):
     return dep*n -diff
 
+def solve(deps):
+    n0 = 1
+    t = n0*deps[0][0]
+    while True:
+        for i in range(1, len(deps)):
+            if not check_condition(t, deps[i][0], deps[i][1]):
+                n0 += 1
+                t = get_t(n0, deps[0][0], deps[0][1])
+                break
+        else:
+            break
+    return t
 
+def solve_next(deps,t,tstep):
+    while True:
+        for i in range(len(deps)):
+            if not check_condition(t, deps[i][0], deps[i][1]):
+                t += tstep
+                break
+        else:
+            break
+    return t
+
+def get_prod(deps):
+    return reduce(lambda a,b:a*b, map(lambda a: a[0], deps))
+
+
+t0 =  1
+tstep = 1
+
+for i in range(2,1+len(deps)):
+    t0 = solve_next(deps[:i], t0, tstep)
+    tstep = get_prod(deps[:i])
+    print(t0)
 
