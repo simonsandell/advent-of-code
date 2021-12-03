85e287983b376674cbf8379123346336759d1673
Simon Sandell <simonsandell94@gmail.com>
2021-12-03 08:01:32 +0100

    day 3 attempt

---
+++ 2021/3/1.py
@@ -0,0 +1,22 @@
+from operator import add
+from functools import reduce
+
+digits = []
+with open('input') as f:
+    for l in f.readlines():
+        d = [int(x) for x in list(l.strip())]
+        digits.append(d)
+
+n = float(len(digits))
+f = lambda x,y : map(add, x, y)
+summed = list(reduce(f, digits))
+
+gamma = list(map(lambda x: round(x/n), summed))
+epsilon = [int(not x) for x in gamma]
+
+gamma = eval("0x" + "".join([str(x) for x in gamma]))
+epsilon = eval("0x" + "".join([str(x) for x in epsilon]))
+
+
+print(gamma*epsilon)
+
