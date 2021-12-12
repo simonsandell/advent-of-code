00586cb7cf7b76afa71319c618075f2f084af78c
Simon Sandell <simonsandell94@gmail.com>
2021-12-12 11:01:10 +0100

    day 12

---
+++ 2021/12/1.py
@@ -0,0 +1,39 @@
+from collections import deque
+
+with open("input") as f:
+    lines = f.read().strip().split("\n")
+
+graph = dict()
+for l in lines:
+    f, t = l.split("-")
+    if t not in graph.keys():
+        graph[t] = set()
+    if f not in graph.keys():
+        graph[f] = set()
+    if t != "start":
+        graph[f].add(t)
+    if f != "start":
+        graph[t].add(f)
+graph["end"] = set()
+
+
+def get_possible_next(path):
+    return graph[path[-1]].difference([x for x in path if x.lower() == x])
+
+
+open_paths = deque()
+for n in graph["start"]:
+    open_paths.append(["start", n])
+
+closed_paths = []
+while len(open_paths) > 0:
+    p = open_paths.pop()
+    nn = get_possible_next(p)
+    if not nn:
+        if p[-1] == "end":
+            closed_paths.append(p)
+    for n in nn:
+        np = p.copy()
+        np.append(n)
+        open_paths.append(np)
+print(len(closed_paths))
