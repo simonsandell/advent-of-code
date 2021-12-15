e87302ac338f3414efb44e8bb9c40b0329cd8e40
Simon Sandell <simonsandell94@gmail.com>
2021-12-15 21:38:24 +0100

    use heapq and prettify gridexpansion

--- 2021/15/2.py
+++ 2021/15/2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import sys
 import numpy as np
+import heapq
 
 infile = sys.argv[1] if len(sys.argv) > 1 else "input"
 
@@ -13,24 +14,21 @@ height = 5 * orig_height
 arr = np.zeros((width, height))
 for y, l in enumerate(lines):
     arr[: len(lines[0]), y] = [int(x) for x in l]
-for i in range(1, 5):
-    arr[orig_width * i : orig_width * (i + 1), :orig_height] = (
-        arr[orig_width * (i - 1) : orig_width * i, :orig_height] + 1
-    )
-    arr[arr > 9] = 1
-for y in range(1, 5):
-    for x in range(0, 5):
-        arr[
-            orig_width * x : orig_width * (x + 1),
-            orig_height * y : orig_height * (y + 1),
-        ] = (
-            arr[
-                orig_width * x : orig_width * (x + 1),
-                orig_height * (y - 1) : orig_height * y,
-            ]
-            + 1
+
+
+def mod(v):
+    if v > 9:
+        return v % 9
+    return v
+
+
+for y in range(height):
+    for x in range(width):
+        arr[x, y] = mod(
+            arr[x % orig_width, y % orig_height]
+            + (x // orig_width)
+            + (y // orig_height)
         )
-        arr[arr > 9] = 1
 
 
 def get_adjacent(k):
@@ -57,25 +55,14 @@ for x in range(width):
         neighbours[(x, y)] = get_adjacent((x, y))
 
 graph = {(0, 0): 0}
-visited = set()
-unvisited = {(0, 0)}
-while True:
-    mv = 10e10
-    mk = None
-    for k in unvisited:
-        if graph[k] < mv:
-            mv = graph[k]
-            mk = k
-    visited.add(mk)
-    unvisited.remove(mk)
+unvisited = [(0, (0, 0))]
+while unvisited:
+    _, mk = heapq.heappop(unvisited)
     if mk == (width - 1, height - 1):
         break
     for n in neighbours[mk]:
-        if n not in visited:
-            unvisited.add(n)
-        if n not in graph:
-            graph[n] = arr[n] + mv
-        else:
-            if arr[n] + mv < graph[n]:
-                graph[n] = arr[n] + mv
+        cost = graph[mk] + arr[n]
+        if n not in graph or cost < graph[n]:
+            heapq.heappush(unvisited, (cost, n))
+            graph[n] = cost
 print(int(graph[(width - 1, height - 1)]))
