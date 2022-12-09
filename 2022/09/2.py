2aa389d343edc1c6bbe7c028bb615b5c77f1237a
Simon Sandell <simonsandell94@gmail.com>
2022-12-10 00:11:14 +0100

    more debug

--- 2022/09/2.py
+++ 2022/09/2.py
@@ -18,9 +18,9 @@ def print_grid(rope, visited):
     grid = [["." for _ in range(max_n)] for _ in range(max_n)]
     grid[max_n // 2][max_n // 2] = "s"
     for v in visited:
-        grid[max_n // 2 + v[0]][max_n // 2 + v[1]] = "#"
+        grid[max_n // 2 + v[1]][max_n // 2 + v[0]] = "#"
     for i, r in enumerate(rope):
-        grid[max_n // 2 + r[0]][max_n // 2 + r[1]] = str(i)
+        grid[max_n // 2 + r[1]][max_n // 2 + r[0]] = str(i)
     for l in grid:
         print("".join(l))
 
@@ -33,7 +33,10 @@ def adjust_t(t_pos, h_pos):
     if x_dist + y_dist == 2 and x_dist != 0 and y_dist != 0:
         return t_pos
     n_pos = [None, None]
-    if h_pos[0] - t_pos[0] >= 2:
+    if x_dist == y_dist == 2:
+        n_pos[0] = h_pos[0] - ((t_pos[0] - h_pos[0]) // (t_pos[0] - h_pos[0]))
+        n_pos[1] = h_pos[1] + ((t_pos[1] - h_pos[1]) // (t_pos[1] - h_pos[1]))
+    elif h_pos[0] - t_pos[0] >= 2:
         n_pos[0] = h_pos[0] - 1
         n_pos[1] = h_pos[1]
     elif h_pos[0] - t_pos[0] <= -2:
@@ -61,28 +64,26 @@ def adjust_rope(rope):
 
 for l in lines:
     to, dist = l.split(" ")
-    if to == "L":
-        for i in range(int(dist)):
+    print(f"== {to} {dist} ==")
+    for i in range(int(dist)):
+        if to == "L":
             rope[0][0] -= 1
             adjust_rope(rope)
             visited.add(tuple(rope[-1]))
-    elif to == "R":
-        for i in range(int(dist)):
+        elif to == "R":
             rope[0][0] += 1
             adjust_rope(rope)
             visited.add(tuple(rope[-1]))
-    if to == "U":
-        for i in range(int(dist)):
+        if to == "U":
             rope[0][1] -= 1
             adjust_rope(rope)
             visited.add(tuple(rope[-1]))
-    elif to == "D":
-        for i in range(int(dist)):
+        elif to == "D":
             rope[0][1] += 1
             adjust_rope(rope)
             visited.add(tuple(rope[-1]))
-    print_grid(rope, visited)
-    print(to, dist)
+        print_grid(rope, visited)
+        print()
     input()
 
 print(len(visited))
