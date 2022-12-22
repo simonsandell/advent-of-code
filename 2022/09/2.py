25cdef43ba68ffdcf12c995f51d6a24ff8371883
Simon Sandell <simonsandell94@gmail.com>
2022-12-22 22:09:30 +0100

    solution part 2

--- 2022/09/2.py
+++ 2022/09/2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import sys
+from copy import deepcopy
 
 infile = sys.argv[1] if len(sys.argv) > 1 else "input"
 
@@ -25,31 +26,50 @@ def print_grid(rope, visited):
         print("".join(l))
 
 
+def check_adjacent(x, y):
+    x_dist = abs(x[0] - y[0])
+    y_dist = abs(x[1] - y[1])
+    if x_dist + y_dist < 2:
+        return True
+    elif x_dist + y_dist == 2 and x_dist != 0 and y_dist != 0:
+        return True
+    return False
+
+
 def adjust_t(t_pos, h_pos):
     x_dist = abs(t_pos[0] - h_pos[0])
     y_dist = abs(t_pos[1] - h_pos[1])
-    if x_dist + y_dist < 2:
-        return t_pos
-    if x_dist + y_dist == 2 and x_dist != 0 and y_dist != 0:
-        return t_pos
-    n_pos = [None, None]
-    if x_dist == y_dist == 2:
-        n_pos[0] = h_pos[0] - ((t_pos[0] - h_pos[0]) // (t_pos[0] - h_pos[0]))
-        n_pos[1] = h_pos[1] + ((t_pos[1] - h_pos[1]) // (t_pos[1] - h_pos[1]))
-    elif h_pos[0] - t_pos[0] >= 2:
-        n_pos[0] = h_pos[0] - 1
-        n_pos[1] = h_pos[1]
-    elif h_pos[0] - t_pos[0] <= -2:
-        n_pos[0] = h_pos[0] + 1
-        n_pos[1] = h_pos[1]
-    elif h_pos[1] - t_pos[1] >= 2:
-        n_pos[1] = h_pos[1] - 1
-        n_pos[0] = h_pos[0]
-    elif h_pos[1] - t_pos[1] <= -2:
-        n_pos[1] = h_pos[1] + 1
-        n_pos[0] = h_pos[0]
-    assert n_pos != [None, None], (t_pos, h_pos)
-    return n_pos
+    n_pos = deepcopy(t_pos)
+    visited = {tuple(n_pos)}
+    if check_adjacent(n_pos, h_pos):
+        return n_pos, visited
+    if x_dist == 0:
+        while not check_adjacent(n_pos, h_pos):
+            if n_pos[1] < h_pos[1]:
+                n_pos[1] = n_pos[1] + 1
+            else:
+                n_pos[1] = n_pos[1] - 1
+            visited.add(tuple(n_pos))
+        return n_pos, visited
+    if y_dist == 0:
+        while not check_adjacent(n_pos, h_pos):
+            if n_pos[0] < h_pos[0]:
+                n_pos[0] = n_pos[0] + 1
+            else:
+                n_pos[0] = n_pos[0] - 1
+            visited.add(tuple(n_pos))
+        return n_pos, visited
+    while not check_adjacent(n_pos, h_pos):
+        if n_pos[0] < h_pos[0]:
+            n_pos[0] = n_pos[0] + 1
+        else:
+            n_pos[0] = n_pos[0] - 1
+        if n_pos[1] < h_pos[1]:
+            n_pos[1] = n_pos[1] + 1
+        else:
+            n_pos[1] = n_pos[1] - 1
+        visited.add(tuple(n_pos))
+    return n_pos, visited
 
 
 rope = []
@@ -59,7 +79,8 @@ for _ in range(10):
 
 def adjust_rope(rope):
     for j in range(1, 10):
-        rope[j] = adjust_t(rope[j], rope[j - 1])
+        rope[j], visited = adjust_t(rope[j], rope[j - 1])
+    return visited
 
 
 for l in lines:
@@ -68,22 +89,16 @@ for l in lines:
     for i in range(int(dist)):
         if to == "L":
             rope[0][0] -= 1
-            adjust_rope(rope)
-            visited.add(tuple(rope[-1]))
         elif to == "R":
             rope[0][0] += 1
-            adjust_rope(rope)
-            visited.add(tuple(rope[-1]))
         if to == "U":
             rope[0][1] -= 1
-            adjust_rope(rope)
-            visited.add(tuple(rope[-1]))
         elif to == "D":
             rope[0][1] += 1
-            adjust_rope(rope)
-            visited.add(tuple(rope[-1]))
-        print_grid(rope, visited)
-        print()
-    input()
+        new_visited = adjust_rope(rope)
+        visited = visited.union(new_visited)
+        # print_grid(rope, visited)
+        # print()
+    # input()
 
 print(len(visited))
