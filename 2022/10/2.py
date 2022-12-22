a2bbe03855e8b12d16740ba5503fd9f157aad0d6
Simon Sandell <simonsandell94@gmail.com>
2022-12-22 22:36:38 +0100

    solution day 10

--- 2022/10/2.py
+++ 2022/10/2.py
@@ -9,16 +9,26 @@ with open(infile) as f:
     lines = f.read().strip().split("\n")
 
 
+screen = [["." for _ in range(40)] for _ in range(6)]
 signal = [1]
+
+
+def draw():
+    xpos = (len(signal) - 1) % 40
+    ypos = (len(signal) - 1) // 40
+    if abs(signal[-1] - xpos) < 2:
+        screen[ypos][xpos] = "#"
 
 
 def addx(x):
     noop()
     signal.append(signal[-1] + x)
+    draw()
 
 
 def noop():
     signal.append(signal[-1])
+    draw()
 
 
 for l in lines:
@@ -27,24 +37,9 @@ for l in lines:
     else:
         addx(int(l.split(" ")[1]))
 
-
-def signal_strength(n):
-    return n * signal[n - 1]
-
-
-print(
-    signal_strength(20)
-    + signal_strength(60)
-    + signal_strength(100)
-    + signal_strength(140)
-    + signal_strength(180)
-    + signal_strength(220)
-)
-print(
-    signal_strength(20),
-    signal_strength(60),
-    signal_strength(100),
-    signal_strength(140),
-    signal_strength(180),
-    signal_strength(220),
-)
+s = ""
+for r in screen:
+    for c in r:
+        s += c
+    s += "\n"
+print(s)
